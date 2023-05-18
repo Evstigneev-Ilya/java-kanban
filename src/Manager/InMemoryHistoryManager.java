32c2cf6101a64b8515a922ff4313437c7223211f
Александра <aleksandra-palagina@mail.ru>
2023-05-18 18:30:48 +0300

    <fix : замечание>

--- src/Manager/InMemoryHistoryManager.java
+++ src/Manager/InMemoryHistoryManager.java
@@ -5,26 +5,24 @@ import Tasks.*;
 import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-
-
-    private CustomLinkedList<Task> historyTask = new CustomLinkedList<>();
+    private Node<Task> head;
+    private Node<Task> tail;
     private Map<Integer, Node> nodeMap = new HashMap<>();
-
-
 
     @Override
     public void add(Task task) {
-        if(historyTask.getSize() == 10){
-            Node<Task> node = historyTask.getHead();
+        if (nodeMap.size() == 10) {
+            Node<Task> node = head;
             nodeMap.remove(node.data.getId());
+            removeNode(node);
         }
-        if(nodeMap.containsKey(task.getId())){
-            historyTask.removeNode(nodeMap.get(task.getId()));
-            historyTask.linkLast(task);
+        if (nodeMap.containsKey(task.getId())) {
+            removeNode(nodeMap.get(task.getId()));
+            linkLast(task);
             nodeMap.put(task.getId(), new Node<>(task));
             return;
         }
-        historyTask.linkLast(task);
+        linkLast(task);
         nodeMap.put(task.getId(), new Node<>(task));
     }
 
@@ -32,7 +30,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     public List<Task> getHistory() {
         List<Task> taskList = new ArrayList<>();
         System.out.println("История ваших задач: ");
-        for(Node<Task> taskNode: nodeMap.values()){
+        for (Node<Task> taskNode : nodeMap.values()) {
             taskList.add(taskNode.data);
         }
         return taskList;
@@ -40,7 +38,44 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void remove(int id) {
-        historyTask.removeNode(nodeMap.get(id));
+        removeNode(nodeMap.get(id));
         nodeMap.remove(id);
     }
+
+
+    public void linkLast(Task element) {
+        Node<Task> addNode = new Node<>(element);
+        if (head == null) {
+            head = addNode;
+            return;
+        }
+        if (tail == null) {
+            tail = addNode;
+            tail.prev = head;
+            return;
+        }
+        tail.next = addNode;
+        tail = addNode;
+
+    }
+
+    public void removeNode(Node<Task> element) {
+        if (element == head) {
+            head.next.prev = null;
+            head = head.next;
+            return;
+        }
+        if (element == tail) {
+            tail.prev.next = null;
+            tail = tail.prev;
+            return;
+        }
+        Node<Task> myHead = head;
+        while (myHead != element) {
+            myHead = myHead.next;
+        }
+        myHead.prev.next = myHead.next;
+        myHead.next.prev = myHead.prev;
+    }
+
 }
--- src/Manager/Node.java
+++ src/Manager/Node.java
@@ -1,20 +1,20 @@
 package Manager;
 
-public class Node <E>{
+public class Node<E> {
 
-        public E data;
-        public Node<E> next;
-        public Node<E> prev;
+    public E data;
+    public Node<E> next;
+    public Node<E> prev;
 
-        public Node(E data) {
-            this.data = data;
+    public Node(E data) {
+        this.data = data;
 
-        }
+    }
 
-        @Override
-        public String toString() {
-                return "Node{" +
-                        "data=" + data +
-                        '}';
-        }
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                '}';
+    }
 }
